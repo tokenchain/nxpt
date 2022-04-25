ae1795a3a8b2520c1be2ef1ad041bf1ff901288a
heskdx <jobhesk@gmail.com>
2022-04-25 15:29:51 +0800

    Check Point 🍥  3 files changed, 13 insertions(+), 1 deletion(-)

---
+++ key.sample.py
@@ -0,0 +1,12 @@
+# main testing account
+
+from moody import conf
+import os
+
+# the public key
+pub = 'xxxxx'
+# the private key
+pri = 'xxxxx'
+
+ROOT = os.path.join(os.path.dirname(__file__))
+NETWORK = conf.RSCMainnet()
